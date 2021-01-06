659b8fd3738a63a3dd245e962518059099c1a932
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2021-01-06 22:37:39 +0800

    整理初始化的目录结构

--- 项目学习/supermall/src/store/index.js
+++ 项目学习/supermall/src/store/index.js
@@ -3,13 +3,16 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+//导入文件
+import state from '@/store/state';
+import mutations from "@/store/mutations";
+import actions from "@/store/actions";
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: state(),
+  mutations,
+  actions,
   modules: {
+
   }
 })
