92b3d79968d5e5c1ff10e64d28f777eb59b30597
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2021-01-07 23:51:24 +0800

    add navbar

---
+++ 项目学习/supermall/src/components/common/navbar/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="nav-bar">
+    <div class="nav-bar-left">
+      <slot name="left"></slot>
+    </div>
+
+    <div class="nav-bar-center" >
+      <slot name="center"></slot>
+    </div>
+
+    <div class="nav-bar-right">
+      <slot name="right"></slot>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+.nav-bar  {
+  display: flex;
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  height: 44px;
+  line-height: 44px;
+}
+.nav-bar-left {
+  width: 60px;
+  /*background-color: blue;*/
+}
+.nav-bar-right{
+  width: 60px;
+  /*background-color: blue;*/
+}
+.nav-bar-center{
+  flex: 1;
+  /*background-color: red;*/
+  text-align: center;
+}
+</style>
--- 项目学习/supermall/src/views/home/Home.vue
+++ 项目学习/supermall/src/views/home/Home.vue
@@ -1,13 +1,27 @@
 <template>
-
+<div>
+  <nav-bar class="home-nav">
+    <div slot="center" >
+      购物车
+    </div>
+  </nav-bar>
+</div>
 </template>
 
 <script>
+import NavBar from "@/components/common/navbar/NavBar";
 export default {
-  name: "Home"
+
+  name: "Home",
+  components:{
+    NavBar,
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav{
+  background-color: var(--color-text);
+  color: #fff;
+}
 </style>
