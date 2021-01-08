b8c9a4ae1edc74a267989a53fdb447d9293ba8c3
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2021-01-08 13:34:36 +0800

    添加主页推荐模块

--- 项目学习/supermall/src/views/home/Home.vue
+++ 项目学习/supermall/src/views/home/Home.vue
@@ -6,30 +6,35 @@
     </div>
   </nav-bar>
   <home-swiper :banners ="banners"></home-swiper>
+  <recommend-view :recommends="recommends"></recommend-view>
 </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
 import HomeSwiper from "@/views/home/childComponents/HomeSwiper";
+import RecommendView from "@/views/home/childComponents/RecommendView"
 import {HomeMultiData} from "@/network/home";
 export default {
 
   name: "Home",
   components:{
     NavBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView
   },
   data(){
     return {
       banners:[],
+      recommends:[],
     }
   },
   created() {
     HomeMultiData().then(res=>{
       console.log("===================")
-     this.banners=res.data.data.banner.list
-      console.log(this.banners)
+      this.banners=res.data.data.banner.list
+      this.recommends = res.data.data.recommend.list
+      console.log(this.recommends)
     })
   }
 }
---
+++ 项目学习/supermall/src/views/home/childComponents/RecommendView.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="recommend-view">
+<div v-for="(item,index) in recommends"  class="recommend-view-item" :key="item.title">
+  <a :href="item.link">
+    <img :src="item.image" alt="">
+  </a>
+
+  <div>{{item.title}}</div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+name: "RecommendView",
+  props:{
+    recommends:{
+      type: Array,
+      default: []
+    }
+  },
+  methods:{
+
+  },
+
+
+}
+</script>
+
+<style scoped>
+.recommend-view{
+  display: flex;
+  position: fixed;
+  left: 0;
+  right: 0;
+}
+.recommend-view-item{
+  flex:1;
+
+}
+.recommend-view-item img{
+  width: 68px;
+  height: 68px;
+  padding-left: 0;
+  padding-right: 0;
+}
+</style>
